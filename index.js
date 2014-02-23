9ef0e5c3d055dce246fdc800f7c9c27565958228
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-22 18:59:35 -0800

    [FEATURE] Add create ripple transaction method implementation.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var models = require('require-all')({
 
 var Adapter = function(){
   this.models = models;
+  this.errors = null;
 }
 
 Adapter.prototype.createExternalAccount = function(opts, fn){
@@ -19,14 +20,30 @@ Adapter.prototype.createExternalAccount = function(opts, fn){
         fn(null, external_account.toJSON());
       } else {
         var error = JSON.parse(JSON.stringify(err));
-        fn({ 
-          name: error.detail, 
+        fn({
+          name: error, 
           user_id: error.detail 
         }, null);
       }
     });
   }
 
+};
+
+Adapter.prototype.createRippleTransaction = function(opts, fn){
+  var model = models.ripple_transaction.build(opts); 
+  var errors = model.validate();
+  if (errors) {
+    fn (errors, null);
+  } else {
+    model.save().complete(function(err, ripple_transaction){
+      if (err) {
+        fn(err, null);
+      } else {
+        fn(null, ripple_transaction);
+      }
+    });
+  }
 };
 
 module.exports = Adapter;

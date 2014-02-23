c34fd4116533292258a6f029a0608efd8a2ae7d5
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-22 19:27:41 -0800

    [FEATURE] Accept very simple payment object.

--- index.js
+++ index.js
@@ -30,8 +30,18 @@ Adapter.prototype.createExternalAccount = function(opts, fn){
 
 };
 
+function fillSimplePayment(simple) {
+  full = simple;
+  full.from_amount = full.from_amount || full.to_amount;
+  full.from_currency = full.from_currency || full.to_currency;
+  full.from_issuer = full.from_issuer || full.to_issuer;
+  return full;
+}
+
 Adapter.prototype.createRippleTransaction = function(opts, fn){
+  opts = fillSimplePayment(opts);
   var model = models.ripple_transaction.build(opts); 
+  
   var errors = model.validate();
   if (opts.to_currency) { opts.to_currency = opts.to_currency.toUpperCase() };
   if (opts.from_currency) { opts.from_currency = opts.from_currency.toUpperCase() };

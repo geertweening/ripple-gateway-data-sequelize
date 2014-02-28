81c4a00bbb78e4ec0dc26db06255a2fd82aca3ce
Steven Zeiler <zeiler.steven@gmail.com>
2014-02-27 22:04:09 -0800

    [FEATURE] Export API, add createUser function.

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 var sequelize = require('./lib/sequelize.js');
+var crypto = require('crypto');
 
 var models = require('require-all')({
   dirname: __dirname + '/lib/models',
   filter: /(.+)\.js(on)?$/
 })
 
-var Adapter = function(){
-  this.models = models;
-  this.errors = null;
-  this.sequelize = sequelize;
+var API = {};
+
+module.exports = function(){
+  API.models = models; 
+  API.sequelize = sequelize;
+  return API;
 }
 
-Adapter.prototype.createExternalAccount = function(opts, fn){
+API.createUser = function(opts, fn) {
+
+  function generateSalt () {
+    var sha = crypto.createHash('sha256');
+    return sha.update(crypto.randomBytes(128)).digest('hex');
+  }
+
+  function saltPassword (password, salt) {
+    return crypto.createHmac('sha256', salt).update(password).digest('hex');
+  }
+
+  function verifyPassword (password, salt, passwordHash) {
+    return (saltPassword(password, salt) == passwordHash);
+  }
+
+  if (opts.name == 'admin') {
+    fn({ name: 'cannot be admin' }, null); return false;
+  }
+
+  opts.salt = generateSalt();
+  opts.password_hash = saltPassword(opts.password, opts.salt);
+  
+  var model = models.user.build(opts);
+
+  var errors = model.validate();
+  if (errors) {
+    fn(errors, null); return false;
+  }
+
+  model.save().complete(function(err, user) {
+    if (err) {
+      fn(err, null);
+    } else {
+      fn(null, user);
+    }
+  });
+};
+
+API.createExternalAccount = function(opts, fn){
   var model = models.external_account.build(opts);
   var errors = model.validate();
 
@@ -33,15 +74,7 @@ Adapter.prototype.createExternalAccount = function(opts, fn){
 
 };
 
-function fillSimplePayment(simple) {
-  full = simple;
-  full.from_amount = full.from_amount || full.to_amount;
-  full.from_currency = full.from_currency || full.to_currency;
-  full.from_issuer = full.from_issuer || full.to_issuer;
-  return full;
-}
-
-Adapter.prototype.createRippleTransaction = function(opts, fn){
+API.createRippleTransaction = function(opts, fn){
   opts = fillSimplePayment(opts);
   var model = models.ripple_transaction.build(opts); 
   
@@ -70,7 +103,7 @@ Adapter.prototype.createRippleTransaction = function(opts, fn){
   }
 };
 
-Adapter.prototype.updateRipplePayment = function(opts, fn){
+API.updateRipplePayment = function(opts, fn){
   models.ripple_transaction.find(opts.id).complete(function(err, transaction){
     if (err) {
       fn(err, null);
@@ -90,7 +123,7 @@ Adapter.prototype.updateRipplePayment = function(opts, fn){
   });
 };
 
-Adapter.prototype.getRipplePayment = function(opts, fn){
+API.getRipplePayment = function(opts, fn){
   models.ripple_transaction.find(opts.id).complete(function(err, payment){
     if (err) {
       fn(err, null);
@@ -102,7 +135,7 @@ Adapter.prototype.getRipplePayment = function(opts, fn){
   });
 };
 
-Adapter.prototype.getRipplePayments = function(opts, fn){
+API.getRipplePayments = function(opts, fn){
   if (typeof opts == 'function') {
     models.ripple_transaction.findAll().complete(fn);
   } else {
@@ -110,7 +143,7 @@ Adapter.prototype.getRipplePayments = function(opts, fn){
   }
 };
 
-Adapter.prototype.deleteRipplePayment = function(opts, fn){
+API.deleteRipplePayment = function(opts, fn){
   models.ripple_transaction.find(opts.id).complete(function(err, transaction){
     if (err) {
       fn(err, null);
@@ -122,7 +155,7 @@ Adapter.prototype.deleteRipplePayment = function(opts, fn){
   });
 }
 
-Adapter.prototype.createRippleAddress = function(opts, fn){
+API.createRippleAddress = function(opts, fn){
   var model = models.ripple_address.build(opts);
   var errors = model.validate();
 
@@ -145,7 +178,7 @@ Adapter.prototype.createRippleAddress = function(opts, fn){
   }); 
 };
 
-Adapter.prototype.getRippleAddress = function(opts, fn){
+API.getRippleAddress = function(opts, fn){
   models.ripple_address.find(opts.id).complete(function(err, ripple_address){
     if(err){
       fn(err, null);
@@ -157,7 +190,7 @@ Adapter.prototype.getRippleAddress = function(opts, fn){
   });
 };
 
-Adapter.prototype.updateRippleAddress = function(opts, fn){
+API.updateRippleAddress = function(opts, fn){
   models.ripple_address.find(opts.id).complete(function(err, ripple_address){
     if (err){
       fn(err, null);
@@ -170,7 +203,7 @@ Adapter.prototype.updateRippleAddress = function(opts, fn){
   });;
 };
 
-Adapter.prototype.deleteRippleAddress = function(opts, fn){
+API.deleteRippleAddress = function(opts, fn){
   models.ripple_address.find(opts.id).complete(function(err, ripple_address){
     data = ripple_address.toJSON();
     ripple_address.destroy().complete(function(){
@@ -179,7 +212,7 @@ Adapter.prototype.deleteRippleAddress = function(opts, fn){
   });
 };
 
-Adapter.prototype.getExternalAccount = function(opts, fn){
+API.getExternalAccount = function(opts, fn){
   models.external_account.find(opts.id).complete(function(err, external_account){
     if (err){
       fn(err, null);
@@ -191,7 +224,7 @@ Adapter.prototype.getExternalAccount = function(opts, fn){
   });
 };
 
-Adapter.prototype.getExternalAccounts = function(opts, fn){
+API.getExternalAccounts = function(opts, fn){
   models.external_account.findAll({ where: opts }).complete(function(err, external_accounts){
     if (err){
       fn(err, null);
@@ -203,7 +236,7 @@ Adapter.prototype.getExternalAccounts = function(opts, fn){
   });
 };
 
-Adapter.prototype.updateExternalAccount = function(opts, fn){
+API.updateExternalAccount = function(opts, fn){
   models.external_account.find(opts.id).complete(function(err, external_account){
     if (err){
       fn(err, null);
@@ -217,7 +250,7 @@ Adapter.prototype.updateExternalAccount = function(opts, fn){
 
 };
 
-Adapter.prototype.deleteExternalAccount = function(opts, fn){
+API.deleteExternalAccount = function(opts, fn){
   models.external_account.find(opts.id).complete(function(err, external_account){
     data = external_account.toJSON();
     external_account.destroy().complete(function(){
@@ -226,7 +259,7 @@ Adapter.prototype.deleteExternalAccount = function(opts, fn){
   });
 };
 
-Adapter.prototype.createExternalTransaction = function(opts, fn){
+API.createExternalTransaction = function(opts, fn){
   var external_transaction = models.external_transaction.build(opts);
   var errors = external_transaction.validate(); 
   if (errors){
@@ -241,7 +274,7 @@ Adapter.prototype.createExternalTransaction = function(opts, fn){
     }
   });
 };
-Adapter.prototype.getExternalTransaction = function(opts, fn){
+API.getExternalTransaction = function(opts, fn){
   models.external_transaction.find(opts.id).complete(function(err, external_transaction){
     if (err){
       fn(err, null);
@@ -253,7 +286,7 @@ Adapter.prototype.getExternalTransaction = function(opts, fn){
   });
 };
 
-Adapter.prototype.updateExternalTransaction = function(opts, fn){
+API.updateExternalTransaction = function(opts, fn){
   models.external_transaction.find(opts.id).complete(function(err, external_transaction){
     if (err){
       fn(err, null);
@@ -267,7 +300,7 @@ Adapter.prototype.updateExternalTransaction = function(opts, fn){
 
 };
 
-Adapter.prototype.deleteExternalTransaction = function(opts, fn){
+API.deleteExternalTransaction = function(opts, fn){
   models.external_transaction.find(opts.id).complete(function(err, external_transaction){
     external_transaction.destroy().complete(function(err, data){
       fn(null, data);
@@ -275,7 +308,7 @@ Adapter.prototype.deleteExternalTransaction = function(opts, fn){
   });
 };
 
-Adapter.prototype.getPendingWithdrawals = function(opts, fn){
+API.getPendingWithdrawals = function(opts, fn){
   var query = {
     status: 'pending',
     deposit: false,
@@ -288,9 +321,6 @@ Adapter.prototype.getPendingWithdrawals = function(opts, fn){
       fn(null, pending_withdrawals || []);
     }
   });
-
 };
-
-module.exports = Adapter;
 
 
